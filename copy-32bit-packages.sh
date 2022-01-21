b46fd9b8ca95de81be730f7e2aade3155f7136fe
Tim Stahlhut <stahta01@gmail.com>
2022-01-21 11:45:18 -0500

    script: Add copy-32bit-packages.sh

---
+++ copy-32bit-packages.sh
@@ -0,0 +1,21 @@
+mkdir -p packages/32bit && \
+cd mingw-org-wsl && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && \
+cd ../mingw-org-zlib && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && 
+cd ../mingw-org-libiconv && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && 
+cd ../mingw-org-gmp && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && 
+cd ../mingw-org-mpfr && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && 
+cd ../mingw-org-mpc && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && 
+cd ../mingw-org-isl && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && 
+cd ../mingw-org-windows-default-manifest && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && 
+cd ../mingw-org-binutils && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/ && \
+cd ../mingw-org-gcc9 && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/32bit/
