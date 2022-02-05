ae13d14ef932cd1ec23da303073405a3f06f5403
Tim Stahlhut <stahta01@gmail.com>
2022-02-05 13:00:47 -0500

    script: Add copy-old_gcc_core_32bit-packages.sh

---
+++ copy-old_gcc_core_32bit-packages.sh
@@ -0,0 +1,11 @@
+mkdir -p packages/mingw-org_core_old_32bit_toolchain && \
+cd mingw-org-wsl3 && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/mingw-org_core_old_32bit_toolchain/ && \
+cd ../mingw-org-zlib && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/mingw-org_core_old_32bit_toolchain/ && \
+cd ../mingw-org-windows-default-manifest && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/mingw-org_core_old_32bit_toolchain/ && \
+cd ../mingw-org-gcc4.4 && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/mingw-org_core_old_32bit_toolchain/ && \
+cd ../mingw-org-binutils-static-libgcc && \
+cp mingw-org-i686*.pkg.tar.zst ../packages/mingw-org_core_old_32bit_toolchain/
