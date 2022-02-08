9990dbb71a10ce188f2c95b795e316a27b9969a6
Tim Stahlhut <stahta01@gmail.com>
2022-02-07 20:48:59 -0500

    script: Change to mingw-org-cross_64bit_toolchain in copy-cross-64bit-packages.sh

--- copy-cross-64bit-packages.sh
+++ copy-cross-64bit-packages.sh
@@ -1,11 +1,11 @@
-mkdir -p packages/cross64bit  && \
+mkdir -p packages/mingw-org-cross_64bit_toolchain  && \
 cd mingw-org-cross-binutils   && \
-cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/cross64bit/ && \
+cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/mingw-org-cross_64bit_toolchain/ && \
 cd ../mingw-org-cross-wsl     && \
-cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/cross64bit/ && \
+cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/mingw-org-cross_64bit_toolchain/ && \
 cd ../mingw-org-cross-gcc     && \
-cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/cross64bit/ && \
+cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/mingw-org-cross_64bit_toolchain/ && \
 cd ../mingw-org-cross-zlib    && \
-cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/cross64bit/ && \
+cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/mingw-org-cross_64bit_toolchain/ && \
 cd ../mingw-org-cross-windows-default-manifest                  && \
-cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/cross64bit/
+cp mingw-org-cross-*-x86_64.pkg.tar.zst ../packages/mingw-org-cross_64bit_toolchain/
